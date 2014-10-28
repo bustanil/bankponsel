b75e3ccdd6f033b4191eb4f06423e56794c62ec8
DaniRosyana <danirosyana@gmail.com>
2014-10-28 17:30:10 +0700

    AllReady

--- src/main/java/id/co/skyforce/bankponsel/service/TransactionService.java
+++ src/main/java/id/co/skyforce/bankponsel/service/TransactionService.java
@@ -3,9 +3,6 @@ package id.co.skyforce.bankponsel.service;
 import java.math.BigDecimal;
 import java.util.Date;
 
-
-
-
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
@@ -22,76 +19,83 @@ import id.co.skyforce.bankponsel.util.HibernateUtil;
 
 public class TransactionService {
 
-	 UserProfile userProfile = new UserProfile();
-	 VirtualAccount virtualAccount; 
-	 static FacesMessage message;
-	
-	 public String deposit(String userAccount, BigDecimal amount){
-		
+	UserProfile userProfile = new UserProfile();
+	VirtualAccount virtualAccount;
+	static FacesMessage message;
+
+	public String deposit(String userAccount, BigDecimal amount) {
+
 		Session session = HibernateUtil.openSession();
 		Transaction trx = session.beginTransaction();
-		virtualAccount = (VirtualAccount) session.get(VirtualAccount.class, userAccount);
+		virtualAccount = (VirtualAccount) session.get(VirtualAccount.class,
+				userAccount);
 		session.close();
-		
+
 		if (virtualAccount == null) {
-			
+
 			message = new FacesMessage("Account Number Not Found");
 			message.setSeverity(FacesMessage.SEVERITY_INFO);
 			FacesContext.getCurrentInstance().addMessage(null, message);
-			
+
 			return "/transaction/deposit";
-		}else{
+		} else {
 			if (amount.doubleValue() > 0) {
-				 session = HibernateUtil.openSession();
-				 trx = session.beginTransaction();
-				
-				virtualAccount.setBalance(virtualAccount.getBalance().add(amount));
-				
+				session = HibernateUtil.openSession();
+				trx = session.beginTransaction();
+
+				virtualAccount.setBalance(virtualAccount.getBalance().add(
+						amount));
+
 				TransactionLog log = new TransactionLog();
 				log.setTransactionDate(new Date());
 				log.setAmount(amount);
 				log.setDbCr('D');
 				log.setVirtualAccount(virtualAccount);
 				session.save(log);
-				
+
 				session.saveOrUpdate(virtualAccount);
 				trx.commit();
 				session.close();
-				
-				message = new FacesMessage("Deposit Succes");
+
+				message = new FacesMessage("Deposit Success");
 				message.setSeverity(FacesMessage.SEVERITY_INFO);
 				FacesContext.getCurrentInstance().addMessage(null, message);
-				
+
 				return "/transaction/deposit";
-			}else{
+			} else {
 				message = new FacesMessage("Deposit Must Greater Than 0");
 				message.setSeverity(FacesMessage.SEVERITY_INFO);
 				FacesContext.getCurrentInstance().addMessage(null, message);
+				
 				return "/transaction/deposit";
 			}
 		}
-		
+
 	}
-	 
-	public String withdrawl(String accountNo,BigDecimal amount){
+
+	public String withdrawl(String accountNo, BigDecimal amount) {
 		Session session = HibernateUtil.openSession();
 		Transaction trx = session.beginTransaction();
-		VirtualAccount virtualAccount = (VirtualAccount) session.get(VirtualAccount.class, accountNo);
+		VirtualAccount virtualAccount = (VirtualAccount) session.get(
+				VirtualAccount.class, accountNo);
 		session.close();
 		if (virtualAccount == null) {
-			
+
 			message = new FacesMessage("Account Number Not Found");
 			message.setSeverity(FacesMessage.SEVERITY_INFO);
 			FacesContext.getCurrentInstance().addMessage(null, message);
-			
+
 			return "/transaction/withdraw";
-		}else{
-			if (virtualAccount.getBalance().doubleValue() > amount.doubleValue() && amount.doubleValue() > 0) { // Check Drawing
+		} else {
+			if (virtualAccount.getBalance().doubleValue() > amount
+					.doubleValue() && amount.doubleValue() > 0) { // Check
+																	// Drawing
 				session = HibernateUtil.openSession();
 				trx = session.beginTransaction();
-				
-				virtualAccount.setBalance(new BigDecimal(virtualAccount.getBalance().doubleValue() - amount.doubleValue()));
-			
+
+				virtualAccount.setBalance(new BigDecimal(virtualAccount
+						.getBalance().doubleValue() - amount.doubleValue()));
+
 				TransactionLog log = new TransactionLog();
 				log.setTransactionDate(new Date());
 				log.setAmount(amount);
@@ -101,46 +105,50 @@ public class TransactionService {
 				session.saveOrUpdate(virtualAccount);
 				trx.commit();
 				session.close();
-				
+
 				message = new FacesMessage("Withdraw Success");
 				message.setSeverity(FacesMessage.SEVERITY_INFO);
 				FacesContext.getCurrentInstance().addMessage(null, message);
 				return "/transaction/withdraw";
-			}else{
+			
+			} else {
 				message = new FacesMessage("Your Withdrawl is Failed");
 				message.setSeverity(FacesMessage.SEVERITY_INFO);
 				FacesContext.getCurrentInstance().addMessage(null, message);
 				return "/transaction/withdraw";
-			}	
-		
+			}
+
 		}
-		
-		
-		
+
 	}
-	
-	public void transfer(String accountNo,String targetAccountNo, BigDecimal amount){
+
+	public void transfer(String accountNo, String targetAccountNo,BigDecimal amount) {
 		Session session = HibernateUtil.openSession();
 		Transaction trx = session.beginTransaction();
-		
-		VirtualAccount account = (VirtualAccount) session.get(VirtualAccount.class, accountNo);
-		VirtualAccount virtualAccount = (VirtualAccount) session.get(VirtualAccount.class, targetAccountNo);
-		if (account.getUserProfile().getUserType().equals('C') && virtualAccount.getUserProfile().getUserType().equals('C')) {
-			
+
+		VirtualAccount account = (VirtualAccount) session.get(
+				VirtualAccount.class, accountNo);
+		VirtualAccount virtualAccount = (VirtualAccount) session.get(
+				VirtualAccount.class, targetAccountNo);
+		if (account.getUserProfile().getUserType().equals('C')
+				&& virtualAccount.getUserProfile().getUserType().equals('C')) {
+
 			if (account.getBalance().doubleValue() > amount.doubleValue()) {
 
-				account.setBalance(new BigDecimal(account.getBalance().doubleValue() - amount.doubleValue()));
+				account.setBalance(new BigDecimal(account.getBalance()
+						.doubleValue() - amount.doubleValue()));
 				session.saveOrUpdate(account);
-				
+
 				TransactionLog log = new TransactionLog();
 				log.setTransactionDate(new Date());
 				log.setAmount(amount);
 				log.setDbCr('C');
 				log.setVirtualAccount(account);
 				session.save(log);
-				
-				virtualAccount.setBalance(new BigDecimal(virtualAccount.getBalance().doubleValue() + amount.doubleValue()));
-			
+
+				virtualAccount.setBalance(new BigDecimal(virtualAccount
+						.getBalance().doubleValue() + amount.doubleValue()));
+
 				log = new TransactionLog();
 				log.setTransactionDate(new Date());
 				log.setAmount(amount);
@@ -150,62 +158,66 @@ public class TransactionService {
 				message = new FacesMessage("Transfer  Success ");
 				message.setSeverity(FacesMessage.SEVERITY_INFO);
 				FacesContext.getCurrentInstance().addMessage(null, message);
-			}else{
+			} else {
 				message = new FacesMessage("Your Balance Not Enough ");
 				message.setSeverity(FacesMessage.SEVERITY_INFO);
 				FacesContext.getCurrentInstance().addMessage(null, message);
 			}
-		
-		}else{
+
+		} else {
 			message = new FacesMessage("Transfer Must Be Member To Member ! ");
 			message.setSeverity(FacesMessage.SEVERITY_INFO);
 			FacesContext.getCurrentInstance().addMessage(null, message);
 		}
-		
+
 		session.saveOrUpdate(virtualAccount);
 		trx.commit();
 		session.close();
 	}
-	
-	public void purchase(String nameProduct, String accountNo){
+
+	public void purchase(String nameProduct, String accountNo) {
 		Session session = HibernateUtil.openSession();
 		Transaction trx = session.beginTransaction();
-		
-		virtualAccount = (VirtualAccount) session.get(VirtualAccount.class, accountNo);
-		
-		Query query = session.createQuery("from Product p where p.name = :name");
+
+		virtualAccount = (VirtualAccount) session.get(VirtualAccount.class,
+				accountNo);
+
+		Query query = session
+				.createQuery("from Product p where p.name = :name");
 		query.setString("name", nameProduct);
 		Product product = (Product) query.uniqueResult();
-		
+
 		double bayar = 0;
-	
+
 		if (virtualAccount.getUserProfile().getUserType().equals('C')) {
-			bayar = product.getPrice().doubleValue() + product.getMargin().doubleValue();
-			virtualAccount.setBalance(new BigDecimal(virtualAccount.getBalance().doubleValue() - bayar));
-		}else if(virtualAccount.getUserProfile().getUserType().equals('M')){
-			bayar = product.getPrice().doubleValue() + (product.getMargin().multiply( product.getSharePercentage()).doubleValue());
-			virtualAccount.setBalance(new BigDecimal(virtualAccount.getBalance().doubleValue() - bayar ));
-			
+			bayar = product.getPrice().doubleValue()
+					+ product.getMargin().doubleValue();
+			virtualAccount.setBalance(new BigDecimal(virtualAccount
+					.getBalance().doubleValue() - bayar));
+		} else if (virtualAccount.getUserProfile().getUserType().equals('M')) {
+			bayar = product.getPrice().doubleValue()
+					+ (product.getMargin().multiply(
+							product.getSharePercentage()).doubleValue());
+			virtualAccount.setBalance(new BigDecimal(virtualAccount
+					.getBalance().doubleValue() - bayar));
+
 		}
 		TransactionLog log = new TransactionLog();
 		log.setDbCr('C');
 		log.setTransactionDate(new Date());
 		log.setAmount(new BigDecimal(bayar));
 		log.setVirtualAccount(getVirtualAccount());
-		
+
 		session.save(log);
 		session.saveOrUpdate(virtualAccount);
-		
-		
+
 		message = new FacesMessage("Purchase Success");
 		message.setSeverity(FacesMessage.SEVERITY_INFO);
 		FacesContext.getCurrentInstance().addMessage(null, message);
-		
+
 		trx.commit();
 		session.close();
 	}
-	
-
 
 	public UserProfile getUserProfile() {
 		return userProfile;
@@ -227,9 +239,6 @@ public class TransactionService {
 		return message;
 	}
 
-	public void setMessage(FacesMessage message) {
-		this.message = message;
-	}
 	
-	
+
 }
